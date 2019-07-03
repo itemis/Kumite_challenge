365bf289f420b7116a176467424c42c43d326925
Alexander Voigt <ahgvoigt@gmail.com>
2019-07-03 13:58:38 +0200

    adding helper function to check for a rectangle at a given position

--- src/matrix.py
+++ src/matrix.py
@@ -18,3 +18,30 @@ class Matrix:
 
     def find_rectangles(self):
         return [0,0,0,0]
+
+    def test_rectangle_at(self, (r,c), (height,width)):
+        """
+        searches for a rectangle at position (r,c) with dimensions (height,width)
+        """
+        if height == 0 or width == 0:
+            return True
+
+        if r + height > self.rows:
+            return False
+        if c + width > self.cols:
+            return False
+
+        # value at (r,c)
+        num = self._matrix[r][c]
+
+        # check whether (r,c) is is valid
+        if num == self._invalid_value:
+            return False
+
+        # check rows/cols for having the same `num'
+        for i in range(height):
+            for k in range(width):
+                if self._matrix[r + i][c + k] != num:
+                    return False
+
+        return True
--- test/test_matrix.py
+++ test/test_matrix.py
@@ -16,8 +16,36 @@ class TestMatrix(unittest.TestCase):
         self.assertEqual(mat.cols, 0)
         self.assertEqual(mat.rows, 0)
 
+        self.assertEqual(mat.test_rectangle_at((0,0), (0,0)), True)
+        self.assertEqual(mat.test_rectangle_at((0,0), (1,1)), False)
+        self.assertEqual(mat.test_rectangle_at((1,1), (1,1)), False)
+
         rectangles = mat.find_rectangles()
         self.assertEqual(rectangles, [0,0,0,0])
+
+    def test_1x4(self):
+        arr = [[1,1,1,1]]
+        mat = matrix.Matrix(arr)
+
+        self.assertEqual(mat.test_rectangle_at((0,0), ((0,0))), True)
+        self.assertEqual(mat.test_rectangle_at((0,0), ((1,1))), True)
+        self.assertEqual(mat.test_rectangle_at((0,0), ((1,2))), True)
+        self.assertEqual(mat.test_rectangle_at((0,0), ((1,3))), True)
+        self.assertEqual(mat.test_rectangle_at((0,0), ((1,4))), True)
+        self.assertEqual(mat.test_rectangle_at((0,0), ((2,1))), False)
+
+    def test_2x2(self):
+        arr = [[1,1],
+               [1,1]]
+        mat = matrix.Matrix(arr)
+
+        self.assertEqual(mat.test_rectangle_at((0,0), ((0,0))), True)
+        self.assertEqual(mat.test_rectangle_at((0,0), ((1,1))), True)
+        self.assertEqual(mat.test_rectangle_at((0,0), ((1,2))), True)
+        self.assertEqual(mat.test_rectangle_at((0,0), ((2,1))), True)
+        self.assertEqual(mat.test_rectangle_at((0,0), ((2,2))), True)
+        self.assertEqual(mat.test_rectangle_at((0,0), ((1,3))), False)
+        self.assertEqual(mat.test_rectangle_at((0,0), ((1,3))), False)
 
 
 if __name__ == '__main__':
